23a2016d55072201e7bacc0b4f322c23d1074c31
AkimT13 <akimtarasov@gmail.com>
2023-11-28 10:18:57 -0800

    almost completed

--- src/Dictionary220.java
+++ src/Dictionary220.java
@@ -1,10 +1,45 @@
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Locale;
+
 /********************************
- * File: []
+ * File: Dictionary220
  * By: Akim Tarasov
- * Date : []
- * Description : []
+ * Date : Nov 27
+ * Description : A dictionary that holds words that are able to contain multiple definitions.
  ********************************/
+
 public class Dictionary220 {
-    public Dictionary220(){}
+        Multimap<String, String> dictionary = ArrayListMultimap.create();
+
+    public Dictionary220(){
+        for(Word word : Word.values()){
+            for(int i=0;i<word.getDefinitions().length;i++){
+                dictionary.put(word.getWordName(),word.getDefinitions()[i]);
+            }
+        }
+
+
+    }
+
+    public void displayDefinitions(String word){
+        ArrayList<String> definitions = new ArrayList<>(dictionary.get(word)); // create an arrayList with a collection as the constructor
+        if(definitions.size()==0){
+            System.out.println("<Not Found>");
+        }
+
+        for(int i =0;i<definitions.size();i++){
+            word = word.substring(0,1).toUpperCase() + word.substring(1);
+            System.out.println( word + ": " + definitions.get(i));
+        }
+
+
+    }
+
+
 
 }
--- src/Dictionary220Driver.java
+++ src/Dictionary220Driver.java
@@ -1,3 +1,4 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 /********************************
@@ -8,18 +9,31 @@ import java.util.Scanner;
  ********************************/
 public class Dictionary220Driver {
     public static void main(String[] args) {
+        System.out.println("- Dictionary 220 Java Standard -----");
+        System.out.println("-----       powered by Google Guava -");
+        Search();
 
 
     }
     public static void Search(){
         Scanner input= new Scanner(System.in);
-        String item = "";
-        while(!item.equals("!q")){
+        boolean searching = true;
+        String item;
+        Dictionary220 dictionary = new Dictionary220();
+        while(searching){
             System.out.print("Search: ");
-            item = input.next();
+
+            item = input.next().toLowerCase();
+            if(item.equals("!q")){
+                searching = false;
+                break;
+            }
+
+            System.out.println("   |");
+            dictionary.displayDefinitions(item);
             System.out.println("   |");
 
-
         }
+        System.out.println("-----THANK YOU-----");
     }
 }
